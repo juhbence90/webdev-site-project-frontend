9783864ecbd2f48bd747e9fce359d591bff945cc
Juhasz Bence <juhbence90@gmail.com>
2026-01-02 15:42:12 +0100

    Fetch post details from Strapi, Homepage posts from Strapi

--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -2,8 +2,13 @@ import type { Route } from "./+types/index";
 import FeaturedProjects from "~/components/FeaturedProjects";
 import AboutPreview from "~/components/AboutPreview";
 import LatestPosts from "~/components/LatestPosts";
-import type { Project, StrapiProject, StrapiResponse } from "~/types";
-import type { PostMeta } from "~/types";
+import type {
+  Project,
+  StrapiPost,
+  StrapiProject,
+  StrapiResponse,
+} from "~/types";
+import type { Post } from "~/types";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -14,14 +19,14 @@ export function meta({}: Route.MetaArgs) {
 
 export async function loader({
   request,
-}: Route.LoaderArgs): Promise<{ projects: Project[]; posts: PostMeta[] }> {
+}: Route.LoaderArgs): Promise<{ projects: Project[]; posts: Post[] }> {
   const url = new URL(request.url);
 
   const [projectRes, postRes] = await Promise.all([
     fetch(
       `${import.meta.env.VITE_API_URL}/projects?filters[featured][$eq]=true&populate=*`
     ),
-    fetch(new URL("/posts-meta.json", url)),
+    fetch(`${import.meta.env.VITE_API_URL}/posts?sort[0]=date:desc&populate=*`),
   ]);
 
   if (!projectRes.ok || !postRes.ok) {
@@ -29,7 +34,7 @@ export async function loader({
   }
 
   const projectJson: StrapiResponse<StrapiProject> = await projectRes.json();
-  const postJson = await postRes.json();
+  const postJson: StrapiResponse<StrapiPost> = await postRes.json();
 
   const projects = projectJson.data.map((item) => ({
     id: item.id,
@@ -45,7 +50,19 @@ export async function loader({
     featured: item.featured,
   }));
 
-  return { projects, posts: postJson };
+  const posts = postJson.data.map((item) => ({
+    id: item.id,
+    title: item.title,
+    slug: item.slug,
+    excerpt: item.excerpt,
+    body: item.body,
+    image: item.image?.url
+      ? `${import.meta.env.VITE_STRAPI_URL}${item.image.url}`
+      : "/images/no-image.png",
+    date: item.date,
+  }));
+
+  return { projects, posts };
 }
 
 const HomePage = ({ loaderData }: Route.ComponentProps) => {
